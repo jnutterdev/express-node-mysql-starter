c50e522005674a1efd9bd85c8fdffa6552968075
jnutterdev <hello@jnutterdev.com>
2019-09-25 11:07:00 +0700

    fixing earlier commit

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Node.js Rest APIs with Express & MySQL example
 
+<<<<<<< HEAD
+=======
+For more detail, please visit:
+> [Build Node.js Rest APIs with Express & MySQL](https://bezkoder.com/node-js-rest-api-express-mysql/)
+
+> [Deploying/Hosting Node.js app on Heroku with MySQL database](https://bezkoder.com/deploy-node-js-app-heroku-cleardb-mysql/)
+
+>>>>>>> 8260dc6 (rebuild for deploying to Heroku & work with ClearDB)
 ## Project setup
 ```
 npm install
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+<<<<<<< HEAD
 const exphbs = require('express-handlebars');
 
 const app = express();
@@ -33,6 +34,21 @@ app.set('view engine', 'hbs');
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: Simple user management system." });
+=======
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+>>>>>>> 8260dc6 (rebuild for deploying to Heroku & work with ClearDB)
 });
 
 require("./app/routes/customer.routes.js")(app);
@@ -40,5 +56,9 @@ require("./app/routes/customer.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+<<<<<<< HEAD
   console.log(`Server is running on http://localhost:${PORT}.`);
+=======
+  console.log(`Server is running on port ${PORT}.`);
+>>>>>>> 8260dc6 (rebuild for deploying to Heroku & work with ClearDB)
 });
