728bd5595673a8734638b92cf1025c1f43b1b0b8
jnutterdev <hello@jnutterdev.com>
2021-03-23 08:14:42 -0400

    Updated some of the configs to a local dev environment

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+const sassMiddleware = require('node-sass-middleware');
+require('dotenv').config();
+
 // parse requests of content-type - application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// Static files
+const path = require('path');
+app.use(sassMiddleware({
+    src: __dirname + '/sass', 
+    dest: __dirname + '/public/stylesheets', 
+    debug: true, 
+    force: true,
+    outputStyle: 'compressed' 
+  }),
+  express.static(path.join(__dirname, 'public')))
+
+// Templating engine
+app.engine('hbs', exphbs({ 
+    extname: 'hbs',
+    defaultLayout: 'main', 
+}));
+app.set('view engine', 'hbs');
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: Simple user management system." });
 });
 
 require("./app/routes/customer.routes.js")(app);
@@ -19,5 +40,5 @@ require("./app/routes/customer.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on http://localhost:${PORT}.`);
 });
